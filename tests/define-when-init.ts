3e55ce9f965b063d70dfdba880324e628b7ce88e
Vladislav <iam.guid@gmail.com>
2022-07-18 21:25:56 +0300

    Improve examples

--- tests/define-when-init.ts
+++ tests/define-when-init.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { FormControl, FormGroup } from "@angular/forms";
-
-const form = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    age: new FormControl<number | null>(null),
-});
 
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
 })
-export class AppComponent implements OnInit {
-    ngOnInit(): void {
-    }
+export class AppComponent {
+    public static form = new FormGroup({
+        firstName: new FormControl(''),
+        lastName: new FormControl(''),
+        email: new FormControl(''),
+        age: new FormControl<number | null>(null),
+    });
 
-    doThomthingWithForm(f: typeof form) {
+    doThomthingWithForm(f: typeof AppComponent.form) {
         f.value.age == 42
     }
 }
